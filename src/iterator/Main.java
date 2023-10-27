a3e5f6571e40d34474a359e0e42367ff4c842e82
leiregesteira <leiregesteira@gmail.com>
2023-10-27 10:10:15 +0200

    Simple Factory

--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -8,11 +8,12 @@ import javax.swing.JTable;
 
 import domain.Covid19Pacient;
 import domain.Symptom;
+import domain.SymptomFactory;
 
 	public class Main {
 
 		public static void main(String[] args) {
-			Covid19Pacient p=new Covid19Pacient("Ane", 29);
+			Covid19Pacient p=new Covid19Pacient("Ane", 29, new SymptomFactory());
 			p.addSymptom(new Symptom("s1", 10, 10), 1);
 			p.addSymptom(new Symptom("s2", 10, 10), 2);
 			p.addSymptom(new Symptom("s3", 10, 10), 3);
